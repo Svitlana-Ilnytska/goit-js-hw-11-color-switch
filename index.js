04771e658b818307f8099dcc99332f8685237da3
Svitlana Ilnytska <svitlanailnicka@gmail.com>
2021-10-24 22:17:04 +0300

    added startChangeColor/stopChangeColor func

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+  start: document.querySelector("[data-action='start']"),
+  stop: document.querySelector("[data-action='stop']"),
+  body: document.querySelector("body"),
+};
+
+let timeInterval = 0;
+
+const startChangeColor = (evt) => {
+  timeInterval = setInterval(() => {
+    let step = randomIntegerFromInterval(0, colors.length);
+    console.log(step);
+
+    document.body.style.backgroundColor = colors[step];
+  }, 1000);
+  refs.start.disabled = true;
+};
+
+const stopChangeColor = (evt) => {
+  refs.start.disabled = false;
+  clearInterval(timeInterval);
+};
+refs.start.addEventListener("click", startChangeColor);
+refs.stop.addEventListener("click", stopChangeColor);
